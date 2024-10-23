4c461bcfc3fbe9fb7f6c057eead29158069bc081
Mateo Abraham <mateoabraham171@gmail.com>
2024-10-23 02:20:29 -0300

    Add slide-out animation for products on purchase and clear cart animation

--- lib/src/presentation/widgets/cart_screen.dart
+++ lib/src/presentation/widgets/cart_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:product_prices/src/presentation/presentation.dart';
 import 'package:product_prices/generated/l10n.dart';
-import 'package:intl/intl.dart'; 
+import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -11,9 +12,81 @@ class CartScreen extends StatefulWidget {
   CartScreenState createState() => CartScreenState();
 }
 
-class CartScreenState extends State<CartScreen> {
+class CartScreenState extends State<CartScreen> with SingleTickerProviderStateMixin {
   bool _isHoveringEmpty = false; 
   bool _isHoveringPurchase = false; 
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  late Animation<Offset> _slideAnimation; // Animación para deslizar
+  bool _isClearing = false; // Para controlar si se está vaciando el carrito
+  bool _isPurchasing = false; // Para controlar si se está comprando
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    // Animación de escala
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    // Animación de deslizamiento hacia la derecha
+    _slideAnimation = Tween<Offset>(begin: Offset.zero, end: const Offset(1.0, 0.0)).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  void _clearCart() {
+    setState(() {
+      _isClearing = true;
+    });
+
+    _controller.forward(); // Iniciamos la animación de vaciar carrito
+    Future.delayed(const Duration(milliseconds: 500), () {
+      if (mounted) {
+        context.read<CartNotifier>().clearCart();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(S.of(context).cartCleared)),
+        );
+        setState(() {
+          _isClearing = false;
+        });
+      }
+    });
+  }
+
+  void _purchase() {
+    setState(() {
+      _isPurchasing = true;
+    });
+
+    _controller.forward(); // Iniciamos la animación de compra
+    Future.delayed(const Duration(milliseconds: 500), () {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(S.of(context).purchaseMade)),
+        );
+        context.read<CartNotifier>().clearCart(); // Vaciar el carrito
+        setState(() {
+          _isPurchasing = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +103,15 @@ class CartScreenState extends State<CartScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(S.of(context).emptyCartMessage),
-                  const SizedBox(height: 16.0),
+                  const SizedBox(height: 24.0),
+                  Lottie.asset(
+                    'assets/lottie/empty_cart.json',
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.contain,
+                    repeat: true,
+                  ),
+                  const SizedBox(height: 24.0),
                   MouseRegion(
                     onEnter: (_) => setState(() => _isHoveringEmpty = true),
                     onExit: (_) => setState(() => _isHoveringEmpty = false),
@@ -42,10 +123,13 @@ class CartScreenState extends State<CartScreen> {
                       onPressed: () {
                         navigator.pop(); 
                       },
-                      child: Text(S.of(context).viewProducts, style: TextStyle(
-                        fontSize: 16.0,
-                        color: _isHoveringEmpty ? Colors.black : Colors.white,
-                      )),
+                      child: Text(
+                        S.of(context).viewProducts,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: _isHoveringEmpty ? Colors.black : Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ],
@@ -60,64 +144,70 @@ class CartScreenState extends State<CartScreen> {
                       final product = cart.items.keys.elementAt(index);
                       final quantity = cart.items[product]!;
 
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: ListTile(
-                          leading: ProductImage(
-                            height: 100.0,
-                            width: 100.0,
-                            tag: product.id.toString(),
-                            url: product.images[0],
-                          ),
-                          title: Text(
-                            product.title,
-                            style: const TextStyle(fontSize: 18.0),
-                          ),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${S.of(context).productDescription} ${NumberFormat.simpleCurrency(locale: Localizations.localeOf(context).toString()).format(product.price)}',
-                                style: const TextStyle(fontSize: 14.0),
+                      return SlideTransition(
+                        position: _isPurchasing ? _slideAnimation : const AlwaysStoppedAnimation(Offset.zero),
+                        child: ScaleTransition(
+                          scale: _isClearing ? _scaleAnimation : const AlwaysStoppedAnimation(1.0),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: ListTile(
+                              leading: ProductImage(
+                                height: 100.0,
+                                width: 100.0,
+                                tag: product.id.toString(),
+                                url: product.images[0],
                               ),
-                              Text(
-                                'Cantidad: $quantity',
-                                style: const TextStyle(fontSize: 14.0),
+                              title: Text(
+                                product.title,
+                                style: const TextStyle(fontSize: 18.0),
                               ),
-                            ],
-                          ),
-                          trailing: SizedBox(
-                            width: 200.0,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Total: ${NumberFormat.simpleCurrency(locale: Localizations.localeOf(context).toString()).format(product.price * quantity)}',
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14.0,
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${S.of(context).productDescription} ${NumberFormat.simpleCurrency(locale: Localizations.localeOf(context).toString()).format(product.price)}',
+                                    style: const TextStyle(fontSize: 14.0),
                                   ),
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
+                                  Text(
+                                    'Cantidad: $quantity',
+                                    style: const TextStyle(fontSize: 14.0),
+                                  ),
+                                ],
+                              ),
+                              trailing: SizedBox(
+                                width: 200.0,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
-                                    IconButton(
-                                      icon: const Icon(Icons.remove_circle),
-                                      onPressed: () {
-                                        cart.removeFromCart(product);
-                                      },
+                                    Text(
+                                      'Total: ${NumberFormat.simpleCurrency(locale: Localizations.localeOf(context).toString()).format(product.price * quantity)}',
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14.0,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
                                     ),
-                                    IconButton(
-                                      icon: const Icon(Icons.add_circle),
-                                      onPressed: () {
-                                        cart.addToCart(product);
-                                      },
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          icon: const Icon(Icons.remove_circle),
+                                          onPressed: () {
+                                            cart.removeFromCart(product);
+                                          },
+                                        ),
+                                        IconButton(
+                                          icon: const Icon(Icons.add_circle),
+                                          onPressed: () {
+                                            cart.addToCart(product);
+                                          },
+                                        ),
+                                      ],
                                     ),
                                   ],
                                 ),
-                              ],
+                              ),
                             ),
                           ),
                         ),
@@ -128,7 +218,7 @@ class CartScreenState extends State<CartScreen> {
                 Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end, 
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       Text(
                         S.of(context).total(NumberFormat.simpleCurrency(locale: Localizations.localeOf(context).toString()).format(cart.totalPrice)),
@@ -136,7 +226,7 @@ class CartScreenState extends State<CartScreen> {
                       ),
                       const SizedBox(height: 16.0),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.end, 
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           MouseRegion(
                             onEnter: (_) => setState(() => _isHoveringEmpty = true),
@@ -146,12 +236,7 @@ class CartScreenState extends State<CartScreen> {
                                 padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
                                 backgroundColor: _isHoveringEmpty ? Colors.red : const Color.fromARGB(0, 33, 149, 243),
                               ),
-                              onPressed: () {
-                                cart.clearCart(); 
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text(S.of(context).cartCleared)),
-                                );
-                              },
+                              onPressed: _clearCart,
                               child: Text(S.of(context).clearCart, style: const TextStyle(fontSize: 16.0)),
                             ),
                           ),
@@ -164,12 +249,7 @@ class CartScreenState extends State<CartScreen> {
                                 padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
                                 backgroundColor: _isHoveringPurchase ? Colors.green : const Color.fromARGB(0, 33, 149, 243),
                               ),
-                              onPressed: () {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text(S.of(context).purchaseMade)),
-                                );
-                                cart.clearCart();
-                              },
+                              onPressed: _purchase, // Llamamos a la función para comprar
                               child: Text(S.of(context).purchase, style: const TextStyle(fontSize: 16.0)),
                             ),
                           ),
